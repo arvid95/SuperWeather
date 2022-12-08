10f758d005f46a91634683623a93be9b60246d11
Arvid <axar1802@student.ju.se>
2022-12-08 10:51:20 +0100

    Initial Commit

---
+++ SuperWeather/SuperWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperWeatherApp.swift
+//  SuperWeather
+//
+//  Created by Arvid Axelsson on 2022-12-08.
+//
+
+import SwiftUI
+
+@main
+struct SuperWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
